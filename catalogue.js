3d20563cdc0feeb9c4976ab55cdb509395cceab4
Dristlelet <workling44@gmail.com>
2026-08-17 00:00:34 +0100

    Add files via upload

---
+++ catalogue.js
@@ -0,0 +1,35 @@
+// Dristlelet Catalogue
+// To update products later, edit the name, price, image and description below.
+
+const catalogue = [
+  {
+    name: "Pink × Purple Hoodie",
+    price: "£55",
+    image: "images/pink-purple-collection.png",
+    description: "Black heavyweight streetwear hoodie with the Pink × Purple Dristlelet design."
+  },
+  {
+    name: "Pink Collection Hoodie",
+    price: "£55",
+    image: "images/pink-collection.png",
+    description: "Pink Dristlelet hoodie with matching front, back and sleeve graphics."
+  },
+  {
+    name: "Drey King Hoodie",
+    price: "£55",
+    image: "images/drey-king.png",
+    description: "Drey King collection hoodie with electric blue graphics."
+  },
+  {
+    name: "Black Lightning Hoodie",
+    price: "£55",
+    image: "images/black-pink-purple.png",
+    description: "Black Dristlelet hoodie with neon pink and purple lightning graphics."
+  },
+  {
+    name: "Blue Voltage Hoodie",
+    price: "£55",
+    image: "images/blue-voltage.png",
+    description: "Black Dristlelet hoodie with electric blue graphics."
+  }
+];
