f53bec29c048bc14c8ee01ee4ed13c1e5a906700
Dristlelet <workling44@gmail.com>
2026-08-17 09:02:22 +0100

    Update catalogue.js

--- catalogue.js
+++ catalogue.js
@@ -1,7 +1,7 @@
 // DRISTLELET catalogue
 // IMPORTANT: Keep the PNG files in the SAME folder as this file.
 // This avoids the GitHub images-folder upload problem we had.
-const catalogue = [
+windows.catalogue = [
 {name:"Pink × Purple Hoodie",price:"£55",image:"pink-purple-collection.png",description:"Black heavyweight streetwear hoodie with the Pink × Purple Dristlelet design."},
 {name:"Pink Collection Hoodie",price:"£55",image:"pink-collection.png",description:"Pink Dristlelet hoodie with matching front, back and sleeve graphics."},
 {name:"Drey King Hoodie",price:"£55",image:"drey-king.png",description:"Drey King collection hoodie with electric blue graphics."},
