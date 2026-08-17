71e07a208929a560ebb5211bc8d8d0b6256a54c1
Dristlelet <workling44@gmail.com>
2026-08-17 13:42:40 +0100

    Update catalogue.js

--- catalogue.js
+++ catalogue.js
@@ -1,10 +1,34 @@
 // DRISTLELET catalogue
-// IMPORTANT: Keep the PNG files in the SAME folder as this file.
-// This avoids the GitHub images-folder upload problem we had.
-windows.catalogue = [
-{name:"Pink × Purple Hoodie",price:"£55",image:"pink-purple-collection.png",description:"Black heavyweight streetwear hoodie with the Pink × Purple Dristlelet design."},
-{name:"Pink Collection Hoodie",price:"£55",image:"pink-collection.png",description:"Pink Dristlelet hoodie with matching front, back and sleeve graphics."},
-{name:"Drey King Hoodie",price:"£55",image:"drey-king.png",description:"Drey King collection hoodie with electric blue graphics."},
-{name:"Black Lightning Hoodie",price:"£55",image:"black-pink-purple.png",description:"Black Dristlelet hoodie with neon pink and purple lightning graphics."},
-{name:"Blue Voltage Hoodie",price:"£55",image:"blue-voltage.png",description:"Black Dristlelet hoodie with electric blue graphics."}
+
+window.catalogue = [
+  {
+    name: "Pink × Purple Hoodie",
+    price: "£55",
+    image: "images/pink-purple-collection.png",
+    description: "Black heavyweight streetwear hoodie with the Pink × Purple Dristlelet design."
+  },
+  {
+    name: "Pink Collection Hoodie",
+    price: "£55",
+    image: "images/pink-collection.png",
+    description: "Pink Dristlelet hoodie with matching front, back and sleeve graphics."
+  },
+  {
+    name: "Drey King Hoodie",
+    price: "£55",
+    image: "images/drey-king.png",
+    description: "Drey King collection hoodie with electric blue graphics."
+  },
+  {
+    name: "Black Lightning Hoodie",
+    price: "£55",
+    image: "images/black-pink-purple.png",
+    description: "Black Dristlelet hoodie with neon pink and purple lightning graphics."
+  },
+  {
+    name: "Blue Voltage Hoodie",
+    price: "£55",
+    image: "images/blue-voltage.png",
+    description: "Black Dristlelet hoodie with electric blue graphics."
+  }
 ];
